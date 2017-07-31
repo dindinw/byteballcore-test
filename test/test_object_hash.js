4f484f0e5d8cb20e43d9c8ba8efacd36c3ded2e3
Alex Wu <dindinw@askcoin.com>
2017-07-31 20:56:39 +0800

    more tests

--- test/test_object_hash.js
+++ test/test_object_hash.js
@@ -1,10 +1,19 @@
 "use strict";
 
 var assert = require('assert');
+
+var constants = require("byteballcore/constants.js");
 var object_hash = require('byteballcore/object_hash.js');
 
 describe("byteballcore/object_hash.js", function() {
 	describe('getBallHash()', function() {
+		var genesis_ball = object_hash.getBallHash(constants.GENESIS_UNIT);
+		it('the genesis_unit is '+constants.GENESIS_UNIT,function() {
+			assert.equal(constants.GENESIS_UNIT,"oj8yEksX9Ubq7lLc+p6F2uyHUuynugeVq4+ikT67X6E=");
+		});
+		it('the genesis_ball is '+genesis_ball,function() {
+            assert.equal("CxK1luSnAk5+MaGyaE9wl26JdwAkSPFDqWJdYs9gRng=",genesis_ball);
+		});
 		it('ball is the hash of the unit ', function() {
 			assert.equal("CxK1luSnAk5+MaGyaE9wl26JdwAkSPFDqWJdYs9gRng=",object_hash.getBallHash("oj8yEksX9Ubq7lLc+p6F2uyHUuynugeVq4+ikT67X6E=",[],[],false));
 		});
